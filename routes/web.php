984b0910147a6fa8984c758ed3ccb33df35d8c3c
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2025-02-08 20:13:44 +0900

    laravel導入、カレンダー導入、todolist導入まで。

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ScheduleController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/calendar', function () {
+        return view('calendar');
+    });
+
+    Route::post('/schedule-add', [ScheduleController::class, 'scheduleAdd'])->name('schedule-add');
+    Route::post('/schedule-get', [ScheduleController::class, 'scheduleGet'])->name('schedule-get');
+
+    Route::get('/todo-list', [ScheduleController::class, 'todoList'])->name('todo-list');
+
+
+});
+
+require __DIR__.'/auth.php';
